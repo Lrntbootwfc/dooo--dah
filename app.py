c41e8af62104420e9c5a75204fdaf9fa838b1c99
dekhte hain <divyasharmagdscoist@gmail.com>
2026-07-02 14:17:28 +0530

    sectional multi pages5

--- app.py
+++ app.py
@@ -1169,4 +1169,5 @@ def _build_initial_seed():
          "mood":"😊","created":"6/27/2026","edited":"6/27/2026","comic":"","stickers":[],"characters":[]}]}]
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
